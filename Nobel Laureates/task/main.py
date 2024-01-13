f5fab244c70198ee96d643984b9599cba6b69257
radosan <radoslaw.sanetra@gmail.com>
2024-01-13 16:54:08 +0100

    adde year_born and age_of_winning columns

--- Nobel Laureates/task/main.py
+++ Nobel Laureates/task/main.py
@@ -3,6 +3,7 @@ import os
 import requests
 import sys
 import numpy as np
+import re
 
 
 def check_data():
@@ -17,16 +18,21 @@ def check_data():
 
 
 def main():
+    # prepare dataset
     check_data()
     df = pd.read_json('../Data/Nobel_laureates.json')
     df.dropna(subset=['gender'], inplace=True, ignore_index=True)
+    # correct birthplace
     df['place_of_birth'] = df['place_of_birth'].apply(lambda x: x.split(',')[-1].strip() if x and ',' in x else np.nan)
     df['born_in'].replace('', np.nan, inplace=True)
     df['born_in'].fillna(df['place_of_birth'], inplace=True)
     df.dropna(subset='born_in', inplace=True, ignore_index=True)
     df.replace(['US', 'United States', 'U.S.'], 'USA', inplace=True)
     df.replace('United Kingdom', 'UK', inplace=True)
-    print(df['born_in'].to_list())
+    # calculate age of winning
+    df['year_born'] = df['date_of_birth'].apply(lambda x: re.search(r'\d{4}', x).group()).astype('int64')
+    df['age_of_winning'] = df['year'] - df['year_born']
+    print(df['year_born'].to_list(), df['age_of_winning'].to_list(), sep='\n')
 
 
 if __name__ == '__main__':
