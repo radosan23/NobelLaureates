0439b9afe5cf172234d17b6621d42636bce7c6aa
radosan <radoslaw.sanetra@gmail.com>
2024-01-14 13:07:41 +0100

    added pie plot of laureates by country

--- Nobel Laureates/task/main.py
+++ Nobel Laureates/task/main.py
@@ -4,6 +4,7 @@ import requests
 import sys
 import numpy as np
 import re
+import matplotlib.pyplot as plt
 
 
 def check_data():
@@ -17,22 +18,39 @@ def check_data():
         sys.stderr.write("[INFO] Loaded.\n")
 
 
-def main():
-    # prepare dataset
-    check_data()
-    df = pd.read_json('../Data/Nobel_laureates.json')
-    df.dropna(subset=['gender'], inplace=True, ignore_index=True)
-    # correct birthplace
+def correct_birthplace(df):
     df['place_of_birth'] = df['place_of_birth'].apply(lambda x: x.split(',')[-1].strip() if x and ',' in x else np.nan)
     df['born_in'].replace('', np.nan, inplace=True)
     df['born_in'].fillna(df['place_of_birth'], inplace=True)
     df.dropna(subset='born_in', inplace=True, ignore_index=True)
     df.replace(['US', 'United States', 'U.S.'], 'USA', inplace=True)
     df.replace('United Kingdom', 'UK', inplace=True)
+    df.drop(columns=['place_of_birth'])
+    return df
+
+
+def plot_countries(df):
+    df['born_in'] = df['born_in'].apply(lambda x: 'Other countries' if df['born_in'].value_counts()[x] < 25 else x)
+    colors = ['blue', 'orange', 'red', 'yellow', 'green', 'pink', 'brown', 'cyan', 'purple']
+    explode = [0, 0, 0, 0.08, 0.08, 0.08, 0.08, 0.08, 0.08]
+    data = df['born_in'].value_counts()
+    fig, ax = plt.subplots(figsize=(12, 12))
+    ax.pie(data, colors=colors, explode=explode, labels=data.index,
+           autopct=lambda pct: f'{pct:.2f}%\n({data.sum()*pct/100:.0f})')
+    plt.show()
+
+
+def main():
+    # prepare dataset
+    check_data()
+    df = pd.read_json('../Data/Nobel_laureates.json')
+    df.dropna(subset=['gender'], inplace=True, ignore_index=True)
+    df = correct_birthplace(df)
     # calculate age of winning
     df['year_born'] = df['date_of_birth'].apply(lambda x: re.search(r'\d{4}', x).group()).astype('int64')
     df['age_of_winning'] = df['year'] - df['year_born']
-    print(df['year_born'].to_list(), df['age_of_winning'].to_list(), sep='\n')
+    # plot laureates by country
+    plot_countries(df)
 
 
 if __name__ == '__main__':
